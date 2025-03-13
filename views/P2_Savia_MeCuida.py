7bf1644bd1cbee1a1c6bf57d59af092dc5512905
= <=>
2025-03-13 13:33:01 -0500

    se conectan los cuatro formualriosdisoinibles por cada programa

--- views/P2_Savia_MeCuida.py
+++ views/P2_Savia_MeCuida.py
@@ -14,13 +14,15 @@ with col1:
     st.image("./assets/SaviaSalud.png", width=230)
 with col2:
     st.title("Proyecto Savia cuida mi corazón", anchor=False)
-    st.write(
-        "Cuidado y bienestar de los riesgos labores y prevención de riesgo cardiovascular"
-    )
+    st.write("Cuidado y bienestar de los riesgos labores y prevención de riesgo cardiovascular")
 
 
 
-
+########################################################################################################################################
+#
+#                                       Lectura de datos 
+#
+########################################################################################################################################
 
 
 gsheetid='1FLzjH7ns7vXf9U3PZ0JQWQOnFRRW7yUM'
@@ -32,105 +34,74 @@ url2 = f'https://docs.google.com/spreadsheets/d/{gsheetid}/export?format=csv&gid
 #st.write(url2)
 dfDatos1 = pd.read_csv(url1)
 m, n = dfDatos1.shape
- 
 st.write(f"Total de personas atendidas en el proyecto: {m:.2f}")
-
-
-
-
-
 dfDatos2 = pd.read_csv(url2)
 
-
+#
+# Datos ANTICOAGULADOS Datos ANTICOAGULADOS Datos ANTICOAGULADOS Datos ANTICOAGULADOS Datos ANTICOAGULADOS Datos ANTICOAGULADOS 
+#
 gsheetid_Anticoagulados='1n869pp61pyY9NgH3THKdafIvsCdvQYHJ6n4r05O60u0'
 sheetid3='296901355#gid=296901355'
 url3 = f'https://docs.google.com/spreadsheets/d/{gsheetid_Anticoagulados}/export?format=csv&gid={sheetid3}'
-dfDatos3 = pd.read_csv(url3)
+dfDatos_P1_Anticoagulados = pd.read_csv(url3)
+
+#
+# Datos INSUFICIENCIA CARDIACA Datos INSUFICIENCIA CARDIACA Datos INSUFICIENCIA CARDIACA Datos INSUFICIENCIA CARDIACA Datos
+#
+gsheetid_InsuficienciaCardiaca='18fKdiLCGWv8bcIdAdTQ6fiJ7CqN13qhVx_u0oy2n-MM'
+sheetid_P1='1062583255#gid=1062583255'
+url_P1 = f'https://docs.google.com/spreadsheets/d/{gsheetid_InsuficienciaCardiaca}/export?format=csv&gid={sheetid_P1}'
+dfDatos_P1_InsuficienciaCardiaca = pd.read_csv(url_P1)
+
+#
+# Datos EPOC Datos EPOC Datos EPOC Datos EPOC Datos EPOC Datos EPOC Datos EPOC Datos EPOC Datos EPOC Datos EPOC Datos
+#
+gsheetid_EPOC='1WPIssU89MljJqsCUpcZU5mKOxw46XXq5AAgVGjgHzbM'
+sheetid_P2='134011359#gid=134011359'
+url_P2 = f'https://docs.google.com/spreadsheets/d/{gsheetid_EPOC}/export?format=csv&gid={sheetid_P2}'
+dfDatos_P2_EPOC = pd.read_csv(url_P2)
+
+#
+# Datos RIESGO CARDIOVASCULAR Datos RIESGO CARDIOVASCULAR Datos RIESGO CARDIOVASCULAR Datos RIESGO CARDIOVASCULAR Datos RIESGO CARDIOVASCULAR
+#
+gsheetid_RiesgoCardiovascular='14Xot90PD6NwSQ8anSRjjigYwtDCm1J_psXSV-y3N02A'
+sheetid_P3='906683604#gid=906683604'
+url_P3 = f'https://docs.google.com/spreadsheets/d/{gsheetid_RiesgoCardiovascular}/export?format=csv&gid={sheetid_P3}'
+dfDatos_P3_RiesgoCardiovascular = pd.read_csv(url_P3)
 
 
-tab1, tab2, tab3, tab4, tab5 = st.tabs(["Insuficiencia Cardiaca", "Riesgo Cardiovascular", "EPOC","Anticoagulados", "Protección Renal"])
+
+
+
+
+tab1, tab2, tab3, tab4 = st.tabs(["Insuficiencia Cardiaca", "Riesgo Cardiovascular", "EPOC","Anticoagulados"])
 
 with tab1:
     st.write("Programa Insuficiencia Cardiaca")
     print(dfDatos2)
-    st.dataframe(dfDatos2, use_container_width=True)
-
+    st.dataframe(dfDatos_P1_InsuficienciaCardiaca, use_container_width=True)
 
 with tab2:
     st.write("Programa Riesgo Cardiovascular")
+    st.dataframe(dfDatos_P3_RiesgoCardiovascular, use_container_width=True)
 
 with tab3:
     st.write("Programa EPOC")
+    st.dataframe(dfDatos_P1_InsuficienciaCardiaca, use_container_width=True)
 
 with tab4:
     st.write("Programa Anticoagulados")
-    print(dfDatos3)
-    st.dataframe(dfDatos3, use_container_width=True)
-
-with tab5:
-    st.write("Programa Protección Renal")
+    print(dfDatos_P1_Anticoagulados)
+    st.dataframe(dfDatos_P1_Anticoagulados, use_container_width=True)
 
 
-
-
-
-st.subheader("Programas de EPOC, Anticuagulados, Riesgo Cardiovascular y Protección Renal", anchor=False)
-st.write("oprima click aqui para acceder al sitio privado del proyecto")
-
-dfDatos = dfDatos1
-print(dfDatos)
-
-st.dataframe(dfDatos, use_container_width=True)
-#st.dataframe(dfDatosTotales, use_container_width=True)
-# Expander básico
-
-
-with st.expander("Haz clic sobre el programa que desees consultar"):
-    st.write("EPOC.")
-    st.write("RCV.")
-    st.image("./assets/corazon.jpg", caption="cuida tu corazón")
-    st.dataframe({
-        "Pacientes": [11, 32, 43],
-        "Programa": ["EPOC", "RCV", "OTRO"]})
-
-st.write(
-    """
-    - Crea formularios
-    - Crea formularios
-    - Procesa datos
-    - Imprime las historias clínicas 
-    - tableros de visualización de la información
-    """
-)
-
-municipios_seleccionados = st.multiselect(
-    "Selecciona los municipios de interés:",  # Texto del widget
-    dfDatos1["MUNICIPIOS"].unique(),     # Opciones (valores únicos de la columna MUNICIPIOS)
-    default=dfDatos1["MUNICIPIOS"].unique()[0]  # Opción seleccionada por defecto (opcional)
-)
-st.write("Has seleccionado los siguientes municipios:", municipios_seleccionados)
-# Filtrar el DataFrame según los municipios seleccionados
-if municipios_seleccionados:
-    df_filtrado = dfDatos1[dfDatos1["MUNICIPIOS"].isin(municipios_seleccionados)]
-    st.write("Datos filtrados:", df_filtrado.shape[0])
-else:
-    st.write("No se ha seleccionado ningún municipio.")
-
-
-#########################################################################################################
-#c1, c2 = st.columns([60, 40])
-#with c1:
-#    dfRegistrosMes = dfDatos1.groupby('REGION').agg({'EDAD':'sum'}).reset_index().sort_values(by='EDAD',ascending=False)
-
-
-import streamlit as st
-import pandas as pd
-import plotly.express as px
+######################################################
+#                                                            https://www.youtube.com/watch?v=1CC9mCzwgK4
+######################################################
 
 # Supongamos que dfDatos1 es tu DataFrame
 # Convertimos la columna FECHA_TOMA_DATO a datetime
 dfDatos1['FECHA_TOMA_DATO'] = pd.to_datetime(dfDatos1['FECHA_TOMA_DATO'], format='%d/%m/%Y')
-
 # Extraemos el mes y el año de la fecha
 dfDatos1['MES'] = dfDatos1['FECHA_TOMA_DATO'].dt.to_period('M')
 
@@ -163,6 +134,43 @@ fig = px.line(
     title=f"Total de registros por región y mes",
     labels={'MES': 'Mes', 'TOTAL': 'Total de registros', 'REGION': 'Región'}
 )
-
 # Mostramos el gráfico en Streamlit
-st.plotly_chart(fig)
+with st.container(border=True):
+    st.plotly_chart(fig)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
